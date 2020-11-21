06636184c20d58b3ed62557b476587336f4d37b0
Clara Jalao <hyde_is_here@hotmail.com>
2020-11-21 12:45:31 +0100

    Updates

---
+++ component---src-pages-404-js-74b58601149e73d02ca1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,n,e){"use strict";e.r(n);e("q1tI");var u=e("Bl7J"),a=e("vrFN"),o=e("qKvR");n.default=function(){return Object(o.a)(u.a,null,Object(o.a)(a.a,{title:"404: Not found"}),Object(o.a)("h1",null,"404: Not Found"),Object(o.a)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-74b58601149e73d02ca1.js.map
